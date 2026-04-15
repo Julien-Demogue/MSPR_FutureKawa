af6621e1491628f0e8e4a79dee56e71d13d8c0fb
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2026-04-15 12:55:59 +0200

    import jest and swagger

--- backend_country/src/main.ts
+++ backend_country/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('FutureKawa - Backend Country')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
