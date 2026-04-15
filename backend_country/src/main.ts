90707abb5b97423e2965faaa8cb746cb91520613
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2026-04-15 16:13:45 +0200

    fix cors error

--- backend_country/src/main.ts
+++ backend_country/src/main.ts
@@ -4,12 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('FutureKawa - Backend Country')
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, documentFactory);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
