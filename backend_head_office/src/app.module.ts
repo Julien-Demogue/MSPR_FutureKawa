abea65e1b68bf88be672a2aba5036b0925a82d0c
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2026-05-04 09:35:33 +0200

    fix port issues

--- backend_head_office/src/app.module.ts
+++ backend_head_office/src/app.module.ts
@@ -12,7 +12,7 @@ import { User } from './users/user.entity';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST ?? 'localhost',
-      port: parseInt(process.env.DB_PORT ?? '3307', 10),
+      port: parseInt(process.env.DB_PORT ?? '3306', 10),
       username: process.env.DB_USER ?? 'root',
       password: process.env.DB_PASSWORD ?? 'root',
       database: process.env.DB_NAME ?? 'FutureKawa_Brasil',
