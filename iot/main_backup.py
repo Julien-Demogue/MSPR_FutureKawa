51ef3957ed5d60d1fb71caa5f02b87b27718d01e
rico517 <128809819+rico517@users.noreply.github.com>
2026-06-28 16:00:23 +0200

    improve iot with retry connection + add backup (#27)
    * improve iot with retry connection + add backup
    
    * update readme

--- iot/main_backup.py
+++ iot/main_backup.py
@@ -12,12 +12,20 @@ wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
 wifi.connect(SSID, PASSWORD)
 
-max_wait = 100
-current_wait = 0
-while not wifi.isconnected() or current_wait < max_wait:
-    print(f"Attempting to connect... ({current_wait})")
+max_wait = 15
+current_wait = max_wait
+while current_wait > 0:
+    if wifi.isconnected():
+        break
+    print(f"Attempting to connect... ({max_wait - current_wait}/{max_wait})")
     time.sleep(1)
-    current_wait += 1
+    current_wait -= 1
+
+if wifi.isconnected():
+    print("Connected to Wi-Fi !")
+    print("Configuring Network:", wifi.ifconfig())
+else:
+    print("Impossible to connect to Wi-Fi")
 
 
 # get ipv4 in a terminal with ipconfig
@@ -27,14 +35,13 @@ port_server = 1883
 # Accessibility test to port 1883 :
 sock = socket.socket()
 print("Connecting to broker...")
-connect_broker = False
-while connect_broker== False:
-    try:
-        sock.connect((mqtt_server, port_server))  
-        print("Connection OK")
-        connect_broker = True
-    except Exception as e:
-        print("Connection Error:", e)
+try:
+    sock.connect((mqtt_server, port_server))  
+    print("Connection OK")
+except Exception as e:
+    print("Connection Error:", e)
+finally:
+    sock.close()
 
 # MQTT
 client_id = "ESP32"
@@ -48,21 +55,6 @@ print("Connected to broker MQTT")
 sensor = dht.DHT11(Pin(32))
 
 while True:
-    if not wifi.isconnected():
-        print("WiFi disconnected, trying to reconnect...")
-        wifi.connect(SSID, PASSWORD)
-        while not wifi.isconnected():
-            time.sleep(1)
-        print("Reconnected to WiFi.")
-    if not client.sock:
-        print("MQTT client disconnected, trying to reconnect...")
-        try:
-            client.connect()
-            print("Reconnected to MQTT broker.")
-        except Exception as e:
-            print("MQTT reconnection error:", e)
-            time.sleep(5)
-            continue
     try:
         sensor.measure() 
         temp = sensor.temperature()  # en °C
@@ -74,4 +66,4 @@ while True:
         print("Data sended ! Temp: {}°C, Hum: {}%".format(temp, hum))
     except Exception as e:
         print("Error:", e)
-    time.sleep(5)
+    time.sleep(2)
